959bf7f8f6fd239ebe861135621424a70b835c5d
Simo Khouili <m.simokhouili@gmail.com>
2019-12-16 21:33:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,26 +1,25 @@
-def using_push(array = colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"], string )
+def using_push(array , string )
 # colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
 array.push "violet"
 end
 
-def using_unshift (array = bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"], string)
-  array.unshift "Staten Island"
+def using_unshift (array , string)
+  array.unshift
 end
 
-def using_pop (array = continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"], string)
-  continents.pop
-end
-using_pop (array, "Antarctica")
-
-
-
-
-
-def using_shift(array = my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"], string)
-  my_favorite_cities.shift
+def using_pop (array , string)
+  array.pop
 end
 
 
-def using_insert (array = list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"], new_element)
-  list_of_programming_languages.insert "Pyton"
+
+
+
+def using_shift(array , string)
+  array.shift
+end
+
+
+def using_insert (arry, new_element)
+  array.insert
 end
